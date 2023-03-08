f97ffeddfcbe1e9091d6a3b657f67f75bcef62d6
tienvx <tien.xuan.vo@gmail.com>
2023-03-09 00:00:42 +0700

    Test service arguments

--- tests/DependencyInjection/TienvxPactProviderExtensionTest.php
+++ tests/DependencyInjection/TienvxPactProviderExtensionTest.php
@@ -3,6 +3,7 @@
 namespace Tienvx\Bundle\MbtBundle\Tests\DependencyInjection;
 
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\DependencyInjection\Argument\ServiceLocatorArgument;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Tienvx\Bundle\PactProviderBundle\DependencyInjection\TienvxPactProviderExtension;
 use Tienvx\Bundle\PactProviderBundle\EventListener\StateChangeRequestListener;
@@ -24,10 +25,15 @@ class TienvxPactProviderExtensionTest extends TestCase
             ['state_change' => ['url' => '/path/to/state/change']],
         ], $this->container);
         $this->assertTrue($this->container->has(StateChangeRequestListener::class));
-        $this->assertTrue(
-            $this->container
-                ->getDefinition(StateChangeRequestListener::class)
-                ->hasTag('kernel.event_listener')
+        $definition = $this->container->getDefinition(StateChangeRequestListener::class);
+        $this->assertTrue($definition->hasTag('kernel.event_listener'));
+        $this->assertInstanceOf(
+            ServiceLocatorArgument::class,
+            $definition->getArgument(0)
+        );
+        $this->assertEquals(
+            '/path/to/state/change',
+            $definition->getArgument(1)
         );
     }
 }
