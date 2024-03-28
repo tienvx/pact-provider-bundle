4a35002115c402876d1c3b900500fba5770e8315
tien.xuan.vo <tien.xuan.vo@gmail.com>
2024-03-28 10:55:25 +0700

    test: Test StateChangeController

--- tests/Application/TestApplication/config/packages/tienvx_pact_provider.php
+++ tests/Application/TestApplication/config/packages/tienvx_pact_provider.php
@@ -2,7 +2,7 @@
 
 $container->loadFromExtension('tienvx_pact_provider', [
     'state_change' => [
-        'body' => true,
+        'body' => '%env(bool:STATE_CHANGE_BODY)%',
         'url' => '/test-pact-change-state',
     ],
     'messages_url' => '/test-pact-messages',
